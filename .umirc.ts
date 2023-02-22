e0e416d38ede3b69d75fdc7ef080ba6f27248a87
xuan <13587013852@163.com>
2023-02-22 21:52:42 +0800

    🎉 docs: create github actions about article

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,11 @@ export default defineConfig({
   base: '/web-learning',
   logo: '/images/xx.png',
   favicon: '/assets/favicon.ico',
+  // navs: [
+  //   {
+  //     title: '前端软技能',
+  //     path: '/softSkills/index'
+  //   }
+  // ]
   // more config: https://d.umijs.org/config
 });
